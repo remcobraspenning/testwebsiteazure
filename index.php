d14f62fab9a65118c487c9f362f68ccbd42e39e4
remcobraspenning <93121194+remcobraspenning@users.noreply.github.com>
2025-06-12 10:52:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 
 echo "Welcome to my World, this is a test for Azure Learning Wortell";
-
-<object width="425" height="350" data="http://www.youtube.com/v/Ahg6qcgoay4" type="application/x-shockwave-flash"><param name="src" value="http://www.youtube.com/v/Ahg6qcgoay4" /></object>
